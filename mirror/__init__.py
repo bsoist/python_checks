0867ad71ecdc8fbdba43fc84261ffefdff7c6351
Bill Soistmann <bsoistmann@THS-MSSCI-BS.local>
2019-10-25 15:46:44 -0400

    Check for mirror

---
+++ mirror/__init__.py
@@ -0,0 +1,38 @@
+import check50
+
+@check50.check()
+def exists():
+    """File mirror.py exists"""
+    check50.exists("mirror.py")
+
+# test(mirror("good") == "gooddoog")
+@check50.check()
+def good():
+    check50.run("python3 mirror.py"
+    ).stdin("good", prompt=False
+    ).stdout("gooddoog", regex=False
+    ).exit()
+
+#test(mirror("Python") == "PythonnohtyP")
+@check50.check()
+def python():
+    check50.run("python3 mirror.py"
+    ).stdin("Python", prompt=False
+    ).stdout("PythonnohtyP", regex=False
+    ).exit()
+
+#test(mirror("") == "")
+@check50.check()
+def empty():
+    check50.run("python3 mirror.py"
+    ).stdin("", prompt=False
+    ).stdout("", regex=False
+    ).exit()
+
+#test(mirror("a") == "aa")
+@check50.check()
+def single():
+    check50.run("python3 mirror.py"
+    ).stdin("a", prompt=False
+    ).stdout("aa", regex=False
+    ).exit()
