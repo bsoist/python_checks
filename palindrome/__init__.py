04e64af8bc801e5a0de72560553ceee62ca33e9c
Bill Soistmann <bsoistmann@THS-MSSCI-BS.local>
2019-10-28 13:44:30 -0400

    Fix redundant function names

--- palindrome/__init__.py
+++ palindrome/__init__.py
@@ -2,12 +2,12 @@ import check50
 
 @check50.check()
 def exists():
-    """File palindrome.py exxxxists"""
+    """File palindrome.py exists"""
     check50.exists("palindrome.py")
 
 #is_palindrome("abba")
 @check50.check()
-def yes_word():
+def yes_abba():
     """Print YES for abba"""
     check50.run("python3 palindrome.py"
         ).stdout("Word? ", regex=False
@@ -18,7 +18,7 @@ def yes_word():
 
 #not is_palindrome("abab")
 @check50.check()
-def no_word():
+def no_abab():
     """Print NO for abab"""
     check50.run("python3 palindrome.py"
         ).stdout("Word? ", regex=False
@@ -29,7 +29,7 @@ def no_word():
 
 #is_palindrome("tenet")
 @check50.check()
-def yes_word():
+def yes_tenet():
     """Print YES for tenet"""
     check50.run("python3 palindrome.py"
         ).stdout("Word? ", regex=False
@@ -40,7 +40,7 @@ def yes_word():
 
 #not is_palindrome("banana")
 @check50.check()
-def no_word():
+def no_banana():
     """Print NO for banana"""
     check50.run("python3 palindrome.py"
         ).stdout("Word? ", regex=False
@@ -50,7 +50,7 @@ def no_word():
 
 #is_palindrome("straw warts")
 @check50.check()
-def yes_word():
+def yes_straw_warts():
     """Print YES for straw warts"""
     check50.run("python3 palindrome.py"
         ).stdout("Word? ", regex=False
@@ -60,7 +60,7 @@ def yes_word():
 
 #is_palindrome("a")
 @check50.check()
-def yes_word():
+def yes_a():
     """Print YES for a"""
     check50.run("python3 palindrome.py"
         ).stdout("Word? ", regex=False
